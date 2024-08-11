fb1ff4eb1de5cf53f6750a334bc48c51a1489d51
newcoder789 <mymail@gmail.com>
2024-08-11 16:01:43 +0530

    Filter added

---
+++ tracker/filters.py
@@ -0,0 +1,15 @@
+import django_filters
+from tracker.models import Transaction  
+
+class TransactionFilter(django_filters.FilterSet):
+    transaction_type = django_filters.ChoiceFilter(
+        choices = Transaction.TRANSACTION_TYPE_CHOICES,
+        field_name = 'type',
+        lookup_expr = 'iexact', #  lookup_expr argument specifies the type of query that should be performed. 
+        #iexact-filter will match values in a case-insensitive manner.
+        empty_label = 'Any',            
+    )
+    
+    class Meta:
+        model = Transaction
+        fields= ('transaction_type',)
